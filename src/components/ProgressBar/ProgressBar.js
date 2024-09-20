b0301b47d187575781ba785914a75bc9e7785a8b
Henry Jones <89033121+henryjonescodes@users.noreply.github.com>
2024-09-20 15:04:15 -0400

    Add date to loading screen

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,24 +1,36 @@
-import React from 'react'
-import { ContentWrapper, ProgressBarPanel, StyledBarWrapper, StyledImage, StyledProgressHeader, StyledProgressPercentage, StyledProgressSubtitle, StyledProgressText, TextWrapper } from './ProgressBarElements'
-const ProgressBar = ({value, max, text, loaded}) => {
-    return (
-        <>
-            <ProgressBarPanel loaded = {loaded}>
-                <ContentWrapper>
-                    <TextWrapper>
-                        <StyledProgressHeader>Henry Jones</StyledProgressHeader>
-                        <StyledProgressSubtitle>Loading Three.js Homepage</StyledProgressSubtitle>
-                    </TextWrapper>
-                    <StyledImage src="/images/avatar.gif" alt="my head... spinning"/>
-                    <StyledBarWrapper>                
-                        <progress value= {value} max ={max}/>
-                        <StyledProgressPercentage>{value}%</StyledProgressPercentage>
-                    </StyledBarWrapper>
-                    <StyledProgressText>{text}</StyledProgressText>
-                </ContentWrapper>
-            </ProgressBarPanel>
-        </>
-    )
-}
+import React from "react";
+import {
+  ContentWrapper,
+  ProgressBarPanel,
+  StyledBarWrapper,
+  StyledImage,
+  StyledProgressHeader,
+  StyledProgressPercentage,
+  StyledProgressSubtitle,
+  StyledProgressText,
+  TextWrapper,
+} from "./ProgressBarElements";
+const ProgressBar = ({ value, max, text, loaded }) => {
+  return (
+    <>
+      <ProgressBarPanel loaded={loaded}>
+        <ContentWrapper>
+          <TextWrapper>
+            <StyledProgressHeader>Henry Jones (2022)</StyledProgressHeader>
+            <StyledProgressSubtitle>
+              Loading Three.js Homepage
+            </StyledProgressSubtitle>
+          </TextWrapper>
+          <StyledImage src="/images/avatar.gif" alt="my head... spinning" />
+          <StyledBarWrapper>
+            <progress value={value} max={max} />
+            <StyledProgressPercentage>{value}%</StyledProgressPercentage>
+          </StyledBarWrapper>
+          <StyledProgressText>{text}</StyledProgressText>
+        </ContentWrapper>
+      </ProgressBarPanel>
+    </>
+  );
+};
 
-export default ProgressBar
+export default ProgressBar;
